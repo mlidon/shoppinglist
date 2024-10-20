3847f9f12a5e273597793e7b210836bb4bbb94a5
mlidon <lidonmarc@gmail.com>
2024-10-20 16:07:02 +0200

    ComponentList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ListComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
